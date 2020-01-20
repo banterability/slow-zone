fb24883685d789608ba4ad2ad697c83c259af4ce
Jeff Long <banterability@users.noreply.github.com>
2020-01-20 11:33:25 -0600

    refactor presenters

---
+++ test/lib/presenters/helpers.js
@@ -0,0 +1,38 @@
+const {
+  getNativeBoolean,
+  getNativeDate,
+  getNativeFloat,
+  getNativeInteger
+} = require("../../../lib/presenters/helpers");
+
+describe("helpers", () => {
+  describe("getNativeBoolean", () => {
+    test("converts an string '1' to a native true", () => {
+      expect(getNativeBoolean("1")).toBe(true);
+    });
+
+    test("converts a string '0' to a native false", () => {
+      expect(getNativeBoolean("0")).toBe(false);
+    });
+  });
+
+  describe("getNativeDate", () => {
+    test("converts an date string to a native date", () => {
+      expect(getNativeDate("2014-10-07T19:49:27.000Z")).toBe(
+        new Date(2014, 11, 7, 19, 49, 27)
+      );
+    });
+  });
+
+  describe("getNativeFloat", () => {
+    test("converts an float string to a native float", () => {
+      expect(getNativeFloat("123.45")).toBe(123.45);
+    });
+  });
+
+  describe("getNativeInteger", () => {
+    test("converts an integer string to a native integer", () => {
+      expect(getNativeInteger("42")).toBe(42);
+    });
+  });
+});
---
+++ test/lib/presenters/route.js
@@ -0,0 +1,55 @@
+const Route = require("../../../lib/presenters/route");
+
+describe("route", () => {
+  describe("sets friendly name for abbreviated routes", () => {
+    test("handles the Brown line", () => {
+      const route = new Route({rt: "Brn"});
+
+      expect(route.route()).toBe("Brown");
+    });
+
+    test("handles the Green line", () => {
+      const route = new Route({rt: "G"});
+
+      expect(route.route()).toBe("Green");
+    });
+
+    test("handles the Orange line", () => {
+      const route = new Route({rt: "Org"});
+
+      expect(route.route()).toBe("Orange");
+    });
+
+    test("handles the Purple line", () => {
+      const route = new Route({rt: "P"});
+
+      expect(route.route()).toBe("Purple");
+    });
+
+    test("handles the Yellow line", () => {
+      const route = new Route({rt: "Y"});
+
+      expect(route.route()).toBe("Yellow");
+    });
+  });
+
+  describe("passes through unabbreviated routes", () => {
+    test("handles the Red line", () => {
+      const route = new Route({rt: "Red"});
+
+      expect(route.route()).toBe("Red");
+    });
+
+    test("handles the Blue line", () => {
+      const route = new Route({rt: "Blue"});
+
+      expect(route.route()).toBe("Blue");
+    });
+
+    test("handles the Pink line", () => {
+      const route = new Route({rt: "Pink"});
+
+      expect(route.route()).toBe("Pink");
+    });
+  });
+});
