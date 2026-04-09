68e39e1c993a8964a07e56513ef5b1e268c962c0
Jeff Long <banterability@users.noreply.github.com>
2026-04-09 15:27:56 -0500

    Slim down ESLint config
    Removes @eslint/compat, @eslint/eslintrc, @eslint/js,
    @typescript-eslint/eslint-plugin, @typescript-eslint/parser,
    eslint-plugin-import, and globals in favor of the unified
    typescript-eslint package and eslint-plugin-import-x, which both
    support flat config natively.

--- eslint.config.js
+++ eslint.config.js
@@ -1,54 +1,18 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import eslintConfigPrettier from "eslint-config-prettier";
+import importX from "eslint-plugin-import-x";
+import tseslint from "typescript-eslint";
 
-import { fixupPluginRules } from "@eslint/compat";
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import _import from "eslint-plugin-import";
-import globals from "globals";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default [
+export default tseslint.config(
+  { ignores: ["dist/"] },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
-    ignores: ["**/dist/"],
-  },
-  ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ),
-  {
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-      import: fixupPluginRules(_import),
-    },
-
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-
-      parser: tsParser,
-    },
-
+    plugins: { "import-x": importX },
     rules: {
-      "import/order": [
+      "import-x/order": [
         "error",
         {
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true,
-          },
-
+          alphabetize: { order: "asc", caseInsensitive: true },
           groups: [
             "builtin",
             "external",
@@ -62,4 +26,4 @@ export default [
       ],
     },
   },
-];
+);
